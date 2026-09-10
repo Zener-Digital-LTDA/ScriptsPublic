2a464aeb8052e0141c01457f1341074a215c3a5c
marcelo-zener <marcelo@zener.digital>
2026-09-10 14:51:04 -0300

    Delete Active-directory/# C:\Scripts\rds_grace.ps1 $dias = (Invoke-CimMethod -InputObject (Get-CimInstance -Namespace root/CIMV2 directory

--- Active-directory/# C:\Scripts\rds_grace.ps1 $dias = (Invoke-CimMethod -InputObject (Get-CimInstance -Namespace root/CIMV2/rds_grace.ps1
+++
@@ -1,3 +0,0 @@
-# C:\Scripts\rds_grace.ps1
-$dias = (Invoke-CimMethod -InputObject (Get-CimInstance -Namespace root/CIMV2/TerminalServices -ClassName Win32_TerminalServiceSetting) -MethodName GetGracePeriodDays).DaysLeft
-$dias | Out-File -FilePath "C:\Scripts\rds_grace.txt" -Encoding ASCII -NoNewline
